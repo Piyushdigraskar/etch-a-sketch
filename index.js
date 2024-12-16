70138db9e9e33ce720670ca8a5a1145a5a74a581
Piyushdigraskar <piyushdigraskar3@gmail.com>
2024-12-16 23:46:40 +0530

    done etch-a-sketch with extra credit 1

--- index.js
+++ index.js
@@ -1,20 +1,38 @@
 const container = document.querySelector('.container');
+const resetButton = document.querySelector("#reset");
 
-function resetBoard () {
-       
+function createGrid(squarePerSide) {
+       container.innerHTML = '';
+
+       const squareSize = 960 / squarePerSide;
+       for (let i = 0; i < squarePerSide * squarePerSide; i++) {
+              const div = document.createElement('div');
+              div.classList = 'grid';
+
+              div.style.width = `${squareSize}px`;
+              div.style.height = `${squareSize}px`;
+
+              div.addEventListener('mouseover', () => {
+                     let r = Math.round(Math.random() * 255);
+                     let g = Math.round(Math.random() * 255);
+                     let b = Math.round(Math.random() * 255);
+                     div.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+              })
+
+              container.appendChild(div);
+       }
 }
 
-for(let i = 0; i < 256;i++){
-       const div = document.createElement('div');
-       div.classList = 'grid';
+resetButton.addEventListener('click', ()=>{
+       const squarePerSide = parseInt(prompt("enter a number between 1 to 100:"));
 
-       div.addEventListener('mouseover', ()=>{
-              let r = Math.round(Math.random() * 255);
-              let g = Math.round(Math.random() * 255);
-              let b = Math.round(Math.random() * 255);
-              div.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-       })
+       if(!squarePerSide || squarePerSide > 100 || squarePerSide < 1){
+              alert("Please enter a valid number between 1 and 100.");
+        return;
+       }
+       createGrid(squarePerSide);
+})
 
-       container.appendChild(div);
-}
+createGrid(16);
+
 
